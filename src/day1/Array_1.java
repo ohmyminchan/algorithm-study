788eb5dc68a9d1cf21b3434edda40b36a017060b
kmc <alscksdl6491@naver.com>
2021-07-28 18:36:02 +0900

    plz

--- src/day1/Array_1.java
+++ src/day1/Array_1.java
@@ -1,10 +1,31 @@
 package day1;
 
+import java.util.Scanner;
+
 public class Array_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println();
+		int X,Y;
+		Scanner sc = new Scanner(System.in);
+		Y = X = sc.nextInt();
+		int[] arr = new int[X];
+		for(int i=0;i<X;i++) {
+			arr[i] = sc.nextInt();
+		}
+		while(Y != 0 ) {
+		for(int i=0;i<X-1;i++) {
+			if(arr[i]<arr[i+1]) {
+				int temp;
+				temp = arr[i];
+				arr[i] = arr[i+1];
+				arr[i+1] = temp;
+			}
+		}
+		Y--;
+		}
+		System.out.println(arr[arr.length-1]+" "+arr[0]);
+		}
+		
 	}
 
-}
+
